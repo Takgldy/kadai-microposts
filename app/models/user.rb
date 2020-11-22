5fb99f587626e8e48a20e28d0d9e590ee719cf39
Tak.gldy <skypole6@gmail.com>
2020-11-22 12:05:46 +0000

    likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,13 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favorings, through: :favorites, source: :micropost
+  
   
   def follow(other_user)
     unless self == other_user
+      
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
   end
@@ -30,4 +34,18 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
+  
+  def favorite(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def favoring?(micropost)
+    self.favorings.include?(micropost)
+  end
+  
 end
